81d6102958b73dc5b9d91414cf8dba717cd65748
John Galt <johngaltfirstrun@gmail.com>
2025-05-05 12:27:57 -0400

    giuliac: vibrator: Introduce configurable effects (richtap/crisp/gentle)
    - Depends on Settings and frameworks/base and sepolicy changes to set
       effects profile
    
    - Gentle effect #5 is not a a typo, gentle doesn't have an effect 5 so use
       crisp here instead as json does.
    
    - Use richtap effects for missing crisp and gentle effects, so only the
       primary effects (0-6) are swapped
    
    [realahnet: Decommonize vibrator profiles]
    
    [0mar99: Mirror configs and effects from waffle
    
    Depends on:
    https://github.com/yaap/vendor_qcom_opensource_vibrator/commits/sixteen/
    https://github.com/yaap/packages_apps_Settings/commit/4aa4a8a28775f5a9b41b979d7e8f077722f2b3df
    https://github.com/yaap/frameworks_base/commit/f9986ac03adda6cb1272de17cc53b62662ce7817 ]
    
    Signed-off-by: 0mar99 <omarag9099@gmail.com>
    Co-Authored-By: ahnet <57623523+realahnet@users.noreply.github.com>

--- device.mk
+++ device.mk
@@ -31,6 +31,10 @@ PRODUCT_PACKAGES += \
     android.hidl.allocator@1.0-service \
     hwservicemanager
 
+# Init
+PRODUCT_PACKAGES += \
+    init.haptics.rc
+
 # Keymint
 PRODUCT_PACKAGES += \
     android.hardware.security.keymint3-service.strongbox.nxp \
@@ -66,8 +70,10 @@ $(call soong_config_set_bool,OPLUS_LINEAGE_TOUCH_HAL,ENABLE_HTPR,false)
 PRODUCT_PACKAGES += \
     vendor.qti.hardware.vibrator.service
 
-$(call soong_config_set_bool,qti_vibrator,use_effect_stream,true)
-$(call soong_config_set,qti_vibrator,effect_lib,libqtivibratoreffect.oplus.giuliac)
+$(call soong_config_set,qti_vibrator,use_effect_stream,true)
+$(call soong_config_set,qti_vibrator,use_effect_stream_strength,true)
+$(call soong_config_set,qti_vibrator,use_primitive_effect_stream,true)
+$(call soong_config_set,qti_vibrator,effect_lib,libqtivibratoreffect.oplus_sm8650-richtap)
 
 # WiFi firmware symlinks
 PRODUCT_PACKAGES += \
