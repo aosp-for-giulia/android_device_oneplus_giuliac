25fe3443f6d15aad0a39778eab86bd3d77b4e758
0mar99 <omarag9099@gmail.com>
2026-01-20 22:09:30 -0600

    giuliac: Derp-ify!
    Signed-off-by: 0mar99 <omarag9099@gmail.com>

--- lineage_giuliac.mk
+++ lineage_giuliac.mk
@@ -14,6 +14,15 @@ $(call inherit-product, device/oneplus/giuliac/device.mk)
 # Inherit some common Lineage stuff.
 $(call inherit-product, vendor/lineage/config/common_full_phone.mk)
 
+# DerpFest Flags
+DERPFEST_BUILD_TYPE := Official
+WITH_GMS := true
+
+# Extras
+TARGET_FACE_UNLOCK_SUPPORTED := true
+TARGET_ENABLE_BLUR := true
+TARGET_SUPPORTS_QUICK_TAP := true
+
 PRODUCT_NAME := lineage_giuliac
 PRODUCT_DEVICE := giuliac
 PRODUCT_MANUFACTURER := OnePlus
