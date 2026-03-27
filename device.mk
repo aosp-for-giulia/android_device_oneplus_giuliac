3fef480f7c53535eacc056186a3091808aa69d8c
Hecheng Yu <kde-yyds@qq.com>
2026-03-27 17:00:51 +0800

    giuliac: Add support for OPlus Camera

--- device.mk
+++ device.mk
@@ -43,6 +43,9 @@ PRODUCT_PACKAGES += \
 # LiveDisplay
 $(call soong_config_set_bool,OPLUS_LINEAGE_LIVEDISPLAY_HAL,ENABLE_SE,false)
 
+# OPlus Camera
+$(call inherit-product-if-exists, vendor/oplus/camera/giuliac/opluscamera.mk)
+
 # Overlays
 DEVICE_PACKAGE_OVERLAYS += \
     $(LOCAL_PATH)/overlay-lineage
