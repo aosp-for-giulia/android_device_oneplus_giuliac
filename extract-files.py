a3dc4bf055e4043d25d9712a11b0758b735bc4df
Bruno Martins <bgcngm@gmail.com>
2026-05-30 14:31:37 +0100

    giuliac: De-blob some oplus interfaces
    Change-Id: Ib5723a97566f61834d20130704227b39b93f70ff

--- extract-files.py
+++ extract-files.py
@@ -41,8 +41,6 @@ lib_fixups: lib_fixups_user_type = {
         'libpwirishalwrapper',
         'libtriplecam_optical_zoom_control',
         'libtriplecam_video_optical_zoom',
-        'vendor.oplus.hardware.camera_rfi-V1-ndk',
-        'vendor.oplus.hardware.cammidasservice-V1-ndk',
         'vendor.oplus.hardware.displaycolorfeature-V1-ndk',
         'vendor.pixelworks.hardware.display@1.0',
         'vendor.pixelworks.hardware.display@1.1',
