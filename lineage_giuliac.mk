7361bdae1c20bde6e1e468197d4c9d1ad8821a86
0mar99 <omarag9099@gmail.com>
2026-08-09 19:25:13 -0600

    giuliac: Update from COS 11.F.54
    Change-Id: I4dbccfeccb0eab57151bfc7684d34a04cd72dd32
    Signed-off-by: 0mar99 <omarag9099@gmail.com>

--- lineage_giuliac.mk
+++ lineage_giuliac.mk
@@ -32,8 +32,8 @@ PRODUCT_MODEL := PKG110
 PRODUCT_GMS_CLIENTID_BASE := android-oneplus
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
-    BuildDesc="qssi-user 16 BP2A.250605.015 1780929007121 release-keys" \
-    BuildFingerprint=OnePlus/PKG110/OP5D2BL1:16/UKQ1.231108.001/V.2d1b9a2-8e0775-93314e:user/release-keys \
+    BuildDesc="qssi-user 16 BP2A.250605.015 1783327895491 release-keys" \
+    BuildFingerprint=OnePlus/PKG110/OP5D2BL1:16/UKQ1.231108.001/V.13b17ec_10f5eb5_10fa46a:user/release-keys \
     DeviceName=OP5D2BL1 \
     DeviceProduct=PKG110 \
     SystemDevice=OP5D2BL1 \
